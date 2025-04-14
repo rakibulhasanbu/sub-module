56a4db0444f3e6b482514cea69ad508290c9bfbb
rakibulhasanbu <rakibulhasanbu@gmail.com>
2025-04-14 11:08:54 +0600

    Fix import paths in index.ts for consistency and clarity

--- src/features/hero/index.ts
+++ src/features/hero/index.ts
@@ -1,33 +1,33 @@
-import { Hero01 } from "@/features/hero/hero-01";
-import { Hero02 } from "@/features/hero/hero-02";
-import { Hero03 } from "@/features/hero/hero-03";
-import { Hero04 } from "@/features/hero/hero-04";
-import { Hero05 } from "@/features/hero/hero-05";
-import { Hero06 } from "@/features/hero/hero-06";
-import { Hero07 } from "@/features/hero/hero-07";
+import { Hero01 } from "../../features/hero/hero-01";
+import { Hero02 } from "../../features/hero/hero-02";
+import { Hero03 } from "../../features/hero/hero-03";
+import { Hero04 } from "../../features/hero/hero-04";
+import { Hero05 } from "../../features/hero/hero-05";
+import { Hero06 } from "../../features/hero/hero-06";
+import { Hero07 } from "../../features/hero/hero-07";
 import {
-    hero01Data,
-    hero02Data,
-    hero03Data,
-    hero04Data,
-    hero05Data,
-    hero06Data,
-    hero07Data,
-} from "@/features/hero/data";
+  hero01Data,
+  hero02Data,
+  hero03Data,
+  hero04Data,
+  hero05Data,
+  hero06Data,
+  hero07Data,
+} from "../../features/hero/data";
 
 export {
-    Hero01,
-    hero01Data,
-    Hero02,
-    hero02Data,
-    Hero03,
-    hero03Data,
-    Hero04,
-    hero04Data,
-    Hero05,
-    hero05Data,
-    Hero06,
-    hero06Data,
-    Hero07,
-    hero07Data,
+  Hero01,
+  hero01Data,
+  Hero02,
+  hero02Data,
+  Hero03,
+  hero03Data,
+  Hero04,
+  hero04Data,
+  Hero05,
+  hero05Data,
+  Hero06,
+  hero06Data,
+  Hero07,
+  hero07Data,
 };
