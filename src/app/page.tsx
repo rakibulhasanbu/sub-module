0eaa67d7d0e5c638839df7104a70d2c2d62c431a
rakibulhasanbu <rakibulhasanbu@gmail.com>
2025-04-14 10:20:13 +0600

    Fix import paths in page.tsx for consistency and clarity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,31 @@
-import RenderComponent from "@/components/RenderComponent";
+import RenderComponent from "../components/RenderComponent";
 import {
-    Hero01,
-    hero01Data,
-    Hero02,
-    hero02Data,
-    Hero03,
-    hero03Data,
-    Hero04,
-    hero04Data,
-    Hero05,
-    hero05Data,
-    Hero06,
-    hero06Data,
-    Hero07,
-    hero07Data,
-} from "@/features/hero";
-import { Section } from "@/type";
+  Hero01,
+  hero01Data,
+  Hero02,
+  hero02Data,
+  Hero03,
+  hero03Data,
+  Hero04,
+  hero04Data,
+  Hero05,
+  hero05Data,
+  Hero06,
+  hero06Data,
+  Hero07,
+  hero07Data,
+} from "../features/hero";
+import { Section } from "../type";
 
 export default function Home() {
-    const components = [
-        { comp: Hero01, data: hero01Data as Section },
-        { comp: Hero02, data: hero02Data as Section },
-        { comp: Hero03, data: hero03Data as Section },
-        { comp: Hero04, data: hero04Data as Section },
-        { comp: Hero05, data: hero05Data as Section },
-        { comp: Hero06, data: hero06Data as Section },
-        { comp: Hero07, data: hero07Data as Section },
-    ];
-    return <RenderComponent components={components} />;
+  const components = [
+    { comp: Hero01, data: hero01Data as Section },
+    { comp: Hero02, data: hero02Data as Section },
+    { comp: Hero03, data: hero03Data as Section },
+    { comp: Hero04, data: hero04Data as Section },
+    { comp: Hero05, data: hero05Data as Section },
+    { comp: Hero06, data: hero06Data as Section },
+    { comp: Hero07, data: hero07Data as Section },
+  ];
+  return <RenderComponent components={components} />;
 }
